136ce6895bb186221382d0e709d7e643bc3b6d93
Edgar Rojo <138748167+Redgnar05@users.noreply.github.com>
2025-06-18 13:31:20 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+
+
+from flask import Flask, render_template, jsonify
+import json
+import random
+
+def crear_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        with open("questions.json") as f:
+            questions = json.load(f)
+        selected = random.sample(questions, 10)
+        return render_template("index.html", questions=selected)
+
+    @app.route("/reset")
+    def reset():
+        with open("questions.json") as f:
+            questions = json.load(f)
+        selected = random.sample(questions, 10)
+        return jsonify(selected)
+    return app
+
+if __name__ == "__main__":
+    app = crear_app()
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
